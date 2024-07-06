f760b072dab20ea9704124a5a89674d1400269c6
songhannaa <shn0322@kakao.com>
2024-07-06 08:34:28 +0000

    header, sidebar layout

--- trippass/src/components/Header.js
+++ trippass/src/components/Header.js
@@ -1,16 +1,32 @@
-import React from "react";
+import React , { useState } from "react";
 import { Link } from "react-router-dom";
-import "../styles/layout.css"; // 스타일 추가
+import "../styles/layout.css"; 
+import logo from "../assets/logo.png";
+import notification from "../assets/notification.png";
+import user from "../assets/user.png";
 
 const Header = () => {
+  const [selected, setSelected] = useState('');
+
+  const handleChange = (event) => {
+    setSelected(event.target.value);
+  };
+
   return (
     <div className="header">
-      <div className="logo"><Link to="/">TripPass</Link></div>
+      <div className="logo"><Link to="/"><img src={logo} alt="logo" /></Link></div>
       <ul className="header-links">
-        <li>여행타이틀 드롭다운</li>
-        <li>알림</li>
         <li>
-          <Link to="/user">마이 페이지</Link>
+          <select className="dropdown" id="dropdown" value={selected} onChange={handleChange}>
+            <option value="">여행을 선택해 주세요</option>
+            <option value="option1">option1</option>
+            <option value="option2">option2</option>
+            <option value="option3">option3</option>
+          </select>
+        </li>
+        <li><img src={notification} alt="notification" /></li>
+        <li>
+          <Link to="/user"><img src={user} alt="user profile" /></Link>
         </li>
       </ul>
     </div>
--- trippass/src/components/Sidebar.js
+++ trippass/src/components/Sidebar.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/layout.css"; // 스타일 추가
+import "../styles/layout.css"; 
+import bot from "../assets/bot1.png";
+import dashboardIcon from "../assets/dashboard.png";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-      <div className="sidebar-title">챗봇과 함께 여행 만들기</div>
+      <div className="planInsert">
+        <div className="botProfile">
+          <img src={bot} alt="trippass bot1" />
+        </div>
+        <div className="planInsertText">
+          <div className="description">같이 계획을 만들어요!</div>
+          <div className="planInsertBtn">
+            챗봇 채팅 시작하기
+          </div>
+        </div>
+      </div>
       <ul>
         <li>
-          <Link to="/">Dashboard</Link>
+          <Link to="/"><img src={dashboardIcon} alt="Da  shboard Icon" />Dashboard</Link>
         </li>
         <li>
-          <Link to="/tripPlan">TripPlan</Link>
+          <Link to="/tripPlan"><img src={dashboardIcon} alt="Dashboard Icon" />TripPlan</Link>
         </li>
         <li>
-          <Link to="/tripCrew">TripCrew</Link>
+          <Link to="/tripCrew"><img src={dashboardIcon} alt="Dashboard Icon" />TripCrew</Link>
         </li>
         <li>
-          <Link to="/chat">Chat</Link>
+          <Link to="/chat"><img src={dashboardIcon} alt="Dashboard Icon" />Chat</Link>
         </li>
       </ul>
     </div>
