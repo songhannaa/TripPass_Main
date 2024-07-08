e43a3b3065cf02cf50fab6ba6b8e96d3976ecd70
songhannaa <shn0322@kakao.com>
2024-07-08 07:21:00 +0000

    trippass login logout

--- trippass/src/App.js
+++ trippass/src/App.js
@@ -1,28 +1,32 @@
+// src/App.js
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
 import DashboardPage from "./pages/DashboardPage.js";
 import LoginPage from "./pages/LoginPage.js";
 import TripPlanPage from "./pages/TripPlanPage.js";
 import TripCrewPage from "./pages/TripCrewPage.js";
 import ChatPage from "./pages/ChatPage.js"
-import UserPage from "./pages/Userpage.js"
+import UserPage from "./pages/UserPage.js"
 import SignUpPage from "./pages/SignUpPage.js"
-
+import AuthRoute from "./components/AuthRoute";
 
 function App() {
+  const isAuthenticated = useSelector((state) => state.user.isAuthenticated);
+
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path="/dashboard" element={<DashboardPage />} />
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={isAuthenticated ? <DashboardPage /> : <LoginPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/user" element={<UserPage />} />
-        <Route path="/tripPlan" element={<TripPlanPage />} />
-        <Route path="/tripCrew" element={<TripCrewPage />} />
-        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/dashboard" element={<AuthRoute><DashboardPage /></AuthRoute>} />
+        <Route path="/tripPlan" element={<AuthRoute><TripPlanPage /></AuthRoute>} />
+        <Route path="/tripCrew" element={<AuthRoute><TripCrewPage /></AuthRoute>} />
+        <Route path="/chat" element={<AuthRoute><ChatPage /></AuthRoute>} />
+        <Route path="/user" element={<AuthRoute><UserPage /></AuthRoute>} />
       </Routes>
     </BrowserRouter>
   );
