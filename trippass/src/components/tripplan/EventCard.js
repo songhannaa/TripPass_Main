b3be251a9f69fe3da49665a973fce05ca2f54126
osy0602 <osy01036095318@gmail.com>
2024-07-08 07:32:07 +0000

    tripplan page develop

---
+++ trippass/src/components/tripplan/EventCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './EventCard.css';
+import locationIcon from '../../assets/location-icon.png';
+
+const EventCard = ({ event }) => {
+  return (
+    <div className="event-card">
+      <div className="event-header">
+        <span className="event-title">{event.title}</span>
+        <span className="event-time">{event.time}</span>
+      </div>
+
+      <p className="event-description">{event.description}</p>
+      <p className="location">
+        <img src={locationIcon} alt="Location Icon" className="location-icon" />
+        <small>{event.location}</small>
+      </p>
+    </div>
+  );
+};
+
+export default EventCard;
--- trippass/src/pages/TripPlanPage.js
+++ trippass/src/pages/TripPlanPage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Layout from "../templates/Layout";
-
+import TripPlan from "../components/tripplan/TripPlan";
 
 const TripPlanPage = () => {
   return (
     <Layout>
-
+      <TripPlan />
     </Layout>
   );
 };
