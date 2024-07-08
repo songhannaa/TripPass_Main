296eeef5232402ad6455cff3e17fa85669f8c153
hijyunk <ziggzagg@ajou.ac.kr>
2024-07-08 07:14:56 +0000

    mypage

--- trippass/src/pages/Userpage.js
+++ trippass/src/pages/Userpage.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Layout from "../templates/Layout";
-
+import User from "../components/user/User.js"
 
 const UserPage = () => {
   return (
     <Layout>
-
+      <User />
     </Layout>
   );
 };
