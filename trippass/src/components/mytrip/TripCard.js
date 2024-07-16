2e013971adbee1a125f6b66ffb07c12e3a26322a
hijyunk <ziggzagg@ajou.ac.kr>
2024-07-16 09:05:06 +0000

    deleteTrip

--- trippass/src/components/mytrip/TripCard.js
+++ trippass/src/components/mytrip/TripCard.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { TiDelete } from "react-icons/ti";
 import '../../styles/mytrip.css';
 
-const TripCard = ({ title, startDate, endDate, banner, isHighlighted, onClick }) => {
+const TripCard = ({ title, startDate, endDate, banner, isHighlighted, onClick, onDelete }) => {
   const imageUrl = `data:image/jpeg;base64,${banner}`;
   const imageClass = isHighlighted ? 'TripCard_Image' : 'TripCard_Image TripCard_Image_Dark';
+
+  const handleDeleteClick = () => {
+    if (window.confirm("해당 여행을 삭제하시겠습니까?")) {
+      onDelete();
+    }
+  };
 
   return (
     <div className="TripCard_Card">
@@ -12,12 +18,12 @@ const TripCard = ({ title, startDate, endDate, banner, isHighlighted, onClick })
         <img src={imageUrl} alt={title} className={imageClass} />
         <div className="TripCard_Overlay">
           {!isHighlighted && (
-            <button className='TripCard_DeleteButton'>
+            <button className='TripCard_DeleteButton' onClick={handleDeleteClick}>
               <TiDelete className='TripCard_Delete' />
             </button>
           )}
           <h3 className="TripCard_Title">{title}</h3>
-          <p className="TripCard_Date">{`${startDate} ~ ${endDate}`}</p>
+          <p className="TripCard_Date">{`${startDate} - ${endDate}`}</p>
           {!isHighlighted && (
             <div className="TripCard_MainOverlay">
               <p className="TripCard_MainText" onClick={onClick}>메인으로 설정하기</p>
